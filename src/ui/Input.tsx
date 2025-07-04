83dc0f3601ae7221300669cb3b61342837ae8dcf
k2o <11086104+ksugawara61@users.noreply.github.com>
2025-07-04 21:59:38 +0900

    fix style

--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -4,7 +4,7 @@ import { TextInput } from "react-native";
 export const Input: FC = () => {
   return (
     <TextInput
-      className="ring-2 rounded p-2"
+      className="rounded p-2 border-2"
       placeholder="useless placeholder"
     />
   );
