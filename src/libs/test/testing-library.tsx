4a3a2fc7d3d048ad5e8d4735238a290528ca6916
k2o <11086104+ksugawara61@users.noreply.github.com>
2025-07-05 22:19:07 +0900

    add: eslint

---
+++ src/libs/test/testing-library.tsx
@@ -0,0 +1,49 @@
+import {
+  render as originalRender,
+  renderHook,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react-native";
+import type React from "react";
+import { type Middleware, SWRConfig } from "swr";
+import { suspenseLoadingTestId, TestProvider } from "./TestProvider";
+
+export * from "@testing-library/react-native";
+
+/** renderHook の Suspense 対応用 wrapper */
+export const renderSuspenseHook = async <Result, Props>(
+  renderCallback: (props: Props) => Result,
+  middlewares?: Middleware[],
+) => {
+  const result = renderHook(renderCallback, {
+    wrapper: ({ children }) => (
+      <TestProvider>
+        <SWRConfig value={{ use: middlewares }}>{children}</SWRConfig>
+      </TestProvider>
+    ),
+  });
+  const suspenseElement = screen.queryByTestId(suspenseLoadingTestId);
+  if (suspenseElement) {
+    await waitForElementToBeRemoved(() =>
+      screen.getByTestId(suspenseLoadingTestId),
+    );
+  }
+  return result;
+};
+
+/** TestProvider で wrap した独自の render */
+export const render = (component: React.ReactElement) => {
+  originalRender(<TestProvider>{component}</TestProvider>);
+};
+
+/** render の Suspense 対応用 wrapper */
+export const renderSuspense = async (component: React.ReactElement) => {
+  originalRender(<TestProvider>{component}</TestProvider>);
+  const suspenseElement = screen.queryByTestId(suspenseLoadingTestId);
+  if (suspenseElement) {
+    await waitForElementToBeRemoved(
+      () => screen.getByTestId(suspenseLoadingTestId),
+      { timeout: 5000 },
+    );
+  }
+};
--- src/ui/Input.stories.tsx
+++ src/ui/Input.stories.tsx
@@ -1,20 +1,9 @@
 import type { Meta, StoryObj } from "@storybook/react-native-web-vite";
-
-import { View } from "react-native";
-
 import { Input } from "./Input";
 
 const meta = {
   component: Input,
-  decorators: [
-    (Story) => (
-      <View style={{ flex: 1, alignItems: "flex-start" }}>
-        <Story />
-      </View>
-    ),
-  ],
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ["autodocs"],
+  decorators: [(Story) => <Story />],
 } satisfies Meta<typeof Input>;
 
 export default meta;
