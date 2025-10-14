b961551c9b1970f684fbc56d9979e3ff6585a10c
k2o <11086104+ksugawara61@users.noreply.github.com>
2025-10-14 21:35:29 +0900

    fix: ローカル設定が正しくなかったので修正＆テスト調整

--- graphql/src/libs/test/globalSetup.ts
+++ graphql/src/libs/test/globalSetup.ts
@@ -2,6 +2,7 @@ import { execSync } from "node:child_process";
 
 const setUp = () => {
   process.env.DATABASE_URL = "file:./test.db";
+  process.env.TURSO_DATABASE_URL = "";
   console.log("Database migration starting...");
   execSync("pnpm db:generate", { stdio: "inherit" });
   execSync("pnpm db:migrate", { stdio: "inherit" });
