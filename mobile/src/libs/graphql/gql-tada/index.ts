a3b0ef9ba0a0c8f6dc555ccec42040b4cca5986f
k2o <11086104+ksugawara61@users.noreply.github.com>
2025-10-15 22:33:10 +0900

    feat: scalar の追加

--- mobile/src/libs/graphql/gql-tada/index.ts
+++ mobile/src/libs/graphql/gql-tada/index.ts
@@ -3,6 +3,17 @@ import type { introspection } from "./graphql-env.js";
 
 export const graphql = initGraphQLTada<{
   introspection: introspection;
+  scalars: {
+    UUID: string;
+    Time: string;
+    Date: string;
+    Cursor: string;
+    Datetime: string;
+    BigFloat: number;
+    BigInt: number;
+    JSON: unknown;
+    Opaque: unknown;
+  };
 }>();
 
 export type { FragmentOf, ResultOf, VariablesOf } from "gql.tada";
