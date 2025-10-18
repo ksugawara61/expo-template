23404b6e748ee83c6f46fb47fe8ee8cc0af59378
k2o <11086104+ksugawara61@users.noreply.github.com>
2025-10-18 21:54:32 +0900

    feat: cloudflare worker の設定を追加

--- graphql/pylon.d.ts
+++ graphql/pylon.d.ts
@@ -1,7 +1,11 @@
 import "@getcronit/pylon";
 
 declare module "@getcronit/pylon" {
-  interface Bindings {}
+  interface Bindings {
+    SECRET_KEY: string;
+    TURSO_DATABASE_URL: string;
+    TURSO_AUTH_TOKEN: string;
+  }
 
   interface Variables {}
 }
--- graphql/src/index.ts
+++ graphql/src/index.ts
@@ -1,10 +1,7 @@
 import { app } from "@getcronit/pylon";
-import { serve } from "@hono/node-server";
 
 import { server } from "./server";
 
 export const graphql = server;
 
-serve(app, (info) => {
-  console.log(`Server running at ${info.port}`);
-});
+export default app;
