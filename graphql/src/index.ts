16a0ebf547f5864e1ccbcc3c6684bf2b98a981b3
k2o <11086104+ksugawara61@users.noreply.github.com>
2025-10-18 22:05:42 +0900

    fix: disabled telemetry

--- graphql/src/index.ts
+++ graphql/src/index.ts
@@ -2,6 +2,10 @@ import { app } from "@getcronit/pylon";
 
 import { server } from "./server";
 
+// to opt-out pylon telemetry data
+// ref: https://pylon.cronit.io/docs/telemetry
+process.env.PYLON_TELEMETRY_DISABLED = "1";
+
 export const graphql = server;
 
 export default app;
--- graphql/vitest.config.ts
+++ graphql/vitest.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
     },
     env: {
       NODE_ENV: "test",
+      PYLON_TELEMETRY_DISABLED: "1",
     },
   },
 });
