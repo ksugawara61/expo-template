63f50f4b441e597181de206c912caa233170fa24
k2o <11086104+ksugawara61@users.noreply.github.com>
2025-10-21 22:38:32 +0900

    fix: server.ts

--- graphql/src/server.ts
+++ graphql/src/server.ts
@@ -1,47 +1,24 @@
 import { fetchArticlesUseCase } from "./application/articles/FetchArticlesUseCase";
-import {
-  type CreateBookmarkInput,
-  createBookmarkUseCase,
-} from "./application/bookmarks/CreateBookmarkUseCase";
+import { createBookmarkUseCase } from "./application/bookmarks/CreateBookmarkUseCase";
 import { deleteBookmarkUseCase } from "./application/bookmarks/DeleteBookmarkUseCase";
 import { fetchBookmarkByIdUseCase } from "./application/bookmarks/FetchBookmarkByIdUseCase";
 import { fetchBookmarksUseCase } from "./application/bookmarks/FetchBookmarksUseCase";
-import {
-  type UpdateBookmarkInput,
-  updateBookmarkUseCase,
-} from "./application/bookmarks/UpdateBookmarkUseCase";
-import {
-  type CreateTagInput,
-  createTagUseCase,
-} from "./application/tags/CreateTagUseCase";
+import { updateBookmarkUseCase } from "./application/bookmarks/UpdateBookmarkUseCase";
+import { createTagUseCase } from "./application/tags/CreateTagUseCase";
 import { fetchTagsUseCase } from "./application/tags/FetchTagsUseCase";
 import { requireAuth } from "./middleware/auth";
 
 export const server = {
   Query: {
-    articles: requireAuth(
-      async (offset?: number, limit?: number) =>
-        await fetchArticlesUseCase(offset, limit),
-    ),
-    bookmarks: requireAuth(async () => await fetchBookmarksUseCase()),
-    bookmark: requireAuth(
-      async (id: string) => await fetchBookmarkByIdUseCase(id),
-    ),
-    tags: requireAuth(async () => await fetchTagsUseCase()),
+    articles: requireAuth(fetchArticlesUseCase),
+    bookmarks: requireAuth(fetchBookmarksUseCase),
+    bookmark: requireAuth(fetchBookmarkByIdUseCase),
+    tags: requireAuth(fetchTagsUseCase),
   },
   Mutation: {
-    createBookmark: requireAuth(
-      async (input: CreateBookmarkInput) => await createBookmarkUseCase(input),
-    ),
-    updateBookmark: requireAuth(
-      async (id: string, input: UpdateBookmarkInput) =>
-        await updateBookmarkUseCase(id, input),
-    ),
-    deleteBookmark: requireAuth(
-      async (id: string) => await deleteBookmarkUseCase(id),
-    ),
-    createTag: requireAuth(
-      async (input: CreateTagInput) => await createTagUseCase(input),
-    ),
+    createBookmark: requireAuth(createBookmarkUseCase),
+    updateBookmark: requireAuth(updateBookmarkUseCase),
+    deleteBookmark: requireAuth(deleteBookmarkUseCase),
+    createTag: requireAuth(createTagUseCase),
   },
 };
